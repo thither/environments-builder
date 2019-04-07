8501a6f7f0cc065c058e5f56f3203b89fbd5584a
Kashirin Alex <kashirin.alex@gmail.com>
2019-04-07 09:58:13 +0300

    Add files via upload

--- pip_install.sh
+++ pip_install.sh
@@ -6,6 +6,10 @@
 interpreter=$1;
 pkgs=${@:2};
 
+if [ $pkgs != 'pip' ]; then
+	$interpreter -m pip uninstall -y $pkgs;
+fi;
+
 export VERBOSE=1;
 export LDFLAGS="-DTCMALLOC_MINIMAL -ltcmalloc_minimal -fno-builtin-malloc -fno-builtin-calloc -fno-builtin-realloc -fno-builtin-free"
 export CFLAGS="-flto -fuse-linker-plugin -ffat-lto-objects -O3 -DNDEBUG $LDFLAGS"
